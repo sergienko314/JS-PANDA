339ff20060ebd455ce7d721e08f6ff608f3c7987
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-27 14:38:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import debounce from 'lodash.debounce';
 
 const searchBtn = document.querySelector('[name="startSearch"]');
 
-//fetchEvents().then(({ data }) => console.log(data._embedded.events));
+fetchEvents().then(({ data }) => console.log(data._embedded.events));
 
-//fetchEvents().then(({ data }) => MakeListMarkup(data._embedded.events));
+fetchEvents().then(({ data }) => MakeListMarkup(data._embedded.events));
 
 searchBtn.addEventListener('input', debounce(eventSearchByName, 500));
