ab231ddc3b01ec759cf878bd43c278b4e36fc8b0
Serggiyenko Mykyta <sergyenko1995@gmail.com>
2022-08-29 18:59:09 +0300

    clean Coments

--- src/js/eventSearchByName.js
+++ src/js/eventSearchByName.js
@@ -7,11 +7,8 @@ import { fetchEvents } from './getEventsApi';
 import throttle from 'lodash.throttle';
 import errorPanda from '../templates/errorPanda.hbs';
 const list = document.querySelector('.js-eventList');
-// const searchBtn = document.querySelector('[name="startSearch"]');
-// searchBtn.addEventListener('input', debounce(eventSearchByName, 500));
 
 const selectPanel = document.querySelector('#search-form');
-// 1234124234123523452345234562346
 
 selectPanel.addEventListener('input', debounce(onSearchForm, 1000));
 async function onSearchForm() {
@@ -19,7 +16,6 @@ async function onSearchForm() {
   serchValue = selectPanel[0].value;
   options.params.keyword = serchValue;
   options.params.countryCode = CountriKAY;
-  // console.log(options.params.countryCode);
   if (options.params.countryCode === 'Choose country') {
     options.params.countryCode = '';
   }
@@ -33,50 +29,6 @@ async function onSearchForm() {
     return (list.innerHTML = errorPanda());
   }
 }
-
-// werwertwertewrtwetrywerytwerwergwetrywetrywety
-// async function onChangeCountryCode() {
-//   try {
-//     if (country.value !== '') {
-//       options.params.countryCode = country.value;
-//     }
-//     let countryValue = country.value;
-//     options.params.countryCode = `${countryValue}`;
-
-//     const response = await axios.get(`${BASE_URL}?`, options);
-//     console.log(response);
-//     return response;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// export async function fetchQueryEvents() {
-//   const q = searchBtn.value;
-//   try {
-//     if (country.value !== '') {
-//       options.params.countryCode = country.value;
-//     }
-//     options.params.keyword = `${q}`;
-//     const response = await axios.get(`${BASE_URL}?`, options);
-//     return response;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// export function eventSearchByName() {
-//   list.innerHTML = '';
-
-//   fetchQueryEvents().then(res => {
-//     if (res.data.page.totalElements === 0) {
-//       list.innerHTML = `<p class="no-event">Sorry, no one event find!</p>`;
-//       searchBtn.value = '';
-//     }
-//     MakeListMarkup(res.data._embedded.events);
-//     searchBtn.value = '';
-//   });
-// }
 
 export async function MakeListMarkup(data) {
   list.insertAdjacentHTML('beforeend', EventList(data));
