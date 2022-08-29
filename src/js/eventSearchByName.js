6bc0c7bd2c29cfcab7f1d7e37ed1959d4e41ee10
Diana Lauzhyna <dlauzhyna@gmail.com>
2022-08-30 01:48:57 +0300

    Add pagination

--- src/js/eventSearchByName.js
+++ src/js/eventSearchByName.js
@@ -9,6 +9,16 @@ import errorPanda from '../templates/errorPanda.hbs';
 const list = document.querySelector('.js-eventList');
 
 const selectPanel = document.querySelector('#search-form');
+//DLM>>
+import { createPagination } from './pagination.js';
+
+const pagination = document.querySelector('.pagination');
+
+let currentPage = 1;
+let totalPage = '';
+let recurcycall = 0;
+let searchBox = '';
+//DLM<<
 
 selectPanel.addEventListener('input', debounce(onSearchForm, 1000));
 async function onSearchForm() {
@@ -21,6 +31,15 @@ async function onSearchForm() {
   }
 
   const res = await axios.get(`${BASE_URL}?`, options);
+  //DLM>> Визнаяення загальної кількості сторінок і виклик пагінації.
+  currentPage = 1;
+  if (res.data.page.totalElements >= 994) {
+    totalPage = Math.ceil(994 / 20);
+  } else {
+    totalPage = Math.ceil(res.data.page.totalElements / 20);
+  }
+  createPagination(totalPage, currentPage);
+  //DLM<<
   try {
     list.innerHTML = '';
     MakeListMarkup(res.data._embedded.events);
@@ -35,8 +54,58 @@ export async function MakeListMarkup(data) {
 
   const li = document.querySelector('.event-list');
   await li.addEventListener('click', onEventLiClick);
+
+  //DLM>> Виклик пагінації
+  if (currentPage === 1) {
+    if (recurcycall === 0) {
+      searchEvents();
+    }
+  }
+  createPagination(totalPage, currentPage);
+  //DLM<<
 }
 
 console.log('12');
 console.log('12');
 console.log('12');
+
+//DLM>>
+//Визначення поточної сторінки
+function setCurrentPage(e) {
+  currentPage = Number(e.target.innerHTML);
+  const pageId = e.target.dataset.id;
+  if (pageId !== undefined) {
+    console.log(`pageId: ${pageId}`);
+    searchEvents();
+    createPagination(totalPage, currentPage);
+  }
+}
+
+//Пошук івенту за ключовим словом для підключення пагінації.
+const searchEvents = async () => {
+  recurcycall++;
+  try {
+    console.log('countryCode ' + selectPanel.elements.chooseQuery.value);
+    if (selectPanel.elements.chooseQuery.value.length <= 2) {
+      options.params.countryCode = selectPanel.elements.chooseQuery.value;
+    }
+    options.params.keyword = selectPanel[0].value;
+    options.params.page = currentPage - 1;
+    const events = await fetchEvents();
+    if (events.data.page.totalElements >= 994) {
+      totalPage = Math.ceil(994 / 20);
+    } else {
+      totalPage = Math.ceil(events.data.page.totalElements / 20);
+    }
+    if (totalPage != 0) {
+      console.log(events.data._embedded.events);
+      list.innerHTML = '';
+      MakeListMarkup(events.data._embedded.events);
+    }
+  } catch (error) {
+    console.log(error.message);
+    console.log('Something WRONG!?!');
+  }
+};
+pagination.addEventListener('click', setCurrentPage); //
+//DLM<<
