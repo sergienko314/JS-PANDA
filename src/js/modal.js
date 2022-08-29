c1c61422b924d68ee63a77740f9ed0112b53944e
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-08-29 12:20:10 +0300

    add modal styles

--- src/js/modal.js
+++ src/js/modal.js
@@ -10,3 +10,15 @@ export function onEventLiClick() {
 export function makeModalMarkup(data) {
   modalDiv.insertAdjacentHTML('beforeend', EventItemMarkup(data));
 }
+
+const btnClose = document.querySelector('.modal__btn--close');
+
+btnClose.addEventListener('click', e => {
+  backdrop.classList.toggle('is-hidden');
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    backdrop.classList.add('is-hidden');
+  }
+});
