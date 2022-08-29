2c777a23b7798ff708be789b6ec279a558c83e67
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-29 12:41:40 +0300

    sum

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,39 @@
+import { options, BASE_URL } from './getEventsApi';
+import axios from 'axios';
+import EventItemMarkup from '../templates/EventItemMarkup.hbs';
+//====================== MODAL
+
+const modalDiv = document.querySelector('.modal__markup');
+const backdrop = document.querySelector('.backdrop');
+
+export async function onEventLiClick(e) {
+  e.preventDefault();
+  backdrop.classList.remove('is-hidden');
+  const eventId = e.target.parentNode.parentNode.id;
+  options.params.id = eventId;
+  const res = await fetchEventsById();
+  makeModalMarkup(res);
+  console.log(res);
+}
+
+export function makeModalMarkup(data) {
+  modalDiv.innerHTML = '';
+  modalDiv.insertAdjacentHTML('beforeend', EventItemMarkup(data));
+}
+
+export async function fetchEventsById() {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/${options.params.id}?`,
+      options
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//===================
 const btnClose = document.querySelector('.modal__btn--close');
 
 btnClose.addEventListener('click', e => {
