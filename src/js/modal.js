217633f41da0fe4c241ecc6962ea52a6c1cc912a
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-30 09:50:58 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -62,12 +62,14 @@ const btnClose = document.querySelector('.modal__btn--close');
 
 btnClose.addEventListener('click', e => {
   backdrop.classList.toggle('is-hidden');
+  document.body.style.overflow = 'auto';
   modalDiv.innerHTML = '';
 });
 
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape') {
     backdrop.classList.add('is-hidden');
+    document.body.style.overflow = 'auto';
     modalDiv.innerHTML = '';
   }
 });
