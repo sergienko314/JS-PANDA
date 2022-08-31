6a0b7ebe78ffbd79d2cc6fac759853029b0ad8da
Diana Lauzhyna <dlauzhyna@gmail.com>
2022-08-31 04:08:08 +0300

    addaed preloader

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,7 +1,12 @@
-window.onload = function () { 
-    document.body.classList.add('loaded_hiding'); 
-    window.setTimeout(function () { 
-      document.body.classList.add('loaded'); 
-      document.body.classList.remove('loaded_hiding'); 
-    }, 500); 
-  };
+let preloaderRef = document.getElementById('preloader');
+
+window.addEventListener('load', closePreloader);
+function closePreloader() {
+  setTimeout(() => {
+    document.body.classList.add('loaded_hiding');
+    setTimeout(() => {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 1000);
+  }, 1000);
+}
